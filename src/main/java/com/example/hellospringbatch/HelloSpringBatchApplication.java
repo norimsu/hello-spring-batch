ac18553df6e03bfb7ab62fde2e9c01567b3bd6ba
SANGHYUN LEE <smarthyun88@lotte.net>
2021-10-21 00:26:53 +0900

    Initial commit

---
+++ src/main/java/com/example/hellospringbatch/HelloSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellospringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hellospringbatch/HelloSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellospringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
